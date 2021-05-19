f62e8921617be492ba7ae1dd1cd656bd1efc5aa7
Michal Novotny <xmnovotny@gmail.com>
2021-05-19 20:12:08 +0200

    Initial commit

---
+++ XMNUtils.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XMNUtils
+{
+    using UnityEngine;
+    using System.IO;
+    using VoxelTycoon.Tracks;
+    using VoxelTycoon.UI;
+
+    public static class GameObjectDumper
+    {
+        static private StringWriter _stringWriter;
+
+        static private StringWriter StringWriter {
+            get {
+                StringWriter result;
+                if ((result = _stringWriter) == null)
+                {
+                    result = (_stringWriter = new StringWriter());
+                }
+                return result;
+            }
+        }
+
+        public static string DumpGameObject(GameObject gameObject)
+        {
+            var stringWriter = StringWriter;
+            stringWriter.GetStringBuilder().Clear();
+            DumpGameObjectInternal(gameObject, stringWriter);
+            return stringWriter.ToString();
+        }
+
+        public static void DumpGameObject(GameObject gameObject, TextWriter writer)
+        {
+            DumpGameObjectInternal(gameObject, writer);
+        }
+
+        private static void DumpGameObjectInternal(GameObject gameObject, TextWriter writer, string indent = "  ")
+        {
+            writer.WriteLine("{0}+{1} ({2})", indent, gameObject.name, gameObject.transform.GetType().Name);
+
+            foreach (Component component in gameObject.GetComponents<Component>())
+            {
+                DumpComponent(component, writer, indent + "  ");
+            }
+
+            foreach (Transform child in gameObject.transform)
+            {
+                DumpGameObjectInternal(child.gameObject, writer, indent + "  ");
+            }
+        }
+
+        private static void DumpComponent(Component component, TextWriter writer, string indent)
+        {
+            writer.WriteLine("{0}{1}", indent, (component == null ? "(null)" : component.GetType().Name));
+        }
+    }
+
+    public static class NotificationUtils
+    {
+        public static void ShowVehicleHint(Vehicle vehicle, string message, Color? textColor=null, Color? panelColor=null)
+        {
+            FloatingHint.ShowHint(message, vehicle.HeadPosition.GetValueOrDefault(), textColor.GetValueOrDefault(Color.white), new PanelColor?(new PanelColor(panelColor.GetValueOrDefault(Color.black), 0.4f)));
+        }
+    }
+}
