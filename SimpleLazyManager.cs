f4e5ddc152911a216e83e2a3162da5df005704fb
Michal Novotny <xmnovotny@gmail.com>
2021-07-15 03:58:43 +0200

    Created SimpleLazyManager

---
+++ SimpleLazyManager.cs
@@ -0,0 +1,37 @@
+﻿using JetBrains.Annotations;
+using VoxelTycoon;
+
+namespace XMNUtils
+{
+    public class SimpleLazyManager<T> where T: SimpleLazyManager<T>, new()
+    {
+        private static T _current;
+
+        [NotNull]
+        public static T Current => _current ??= new T();
+        [CanBeNull]
+        public static T CurrentWithoutInit => _current;
+
+        protected UpdateBehaviour Behaviour { get; private set; }
+     
+        protected SimpleLazyManager()
+        {
+            Behaviour = UpdateBehaviour.Create(typeof(T).Name);
+            Behaviour.OnDestroyAction = delegate
+            {
+                OnDeinitialize();
+                _current = null;
+            };
+            OnInitialize();
+        }
+        
+        protected virtual void OnDeinitialize()
+        {
+        }
+     
+        protected virtual void OnInitialize()
+        {
+        }
+        
+    }
+}
